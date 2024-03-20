c56ee4424f076e082ce0579641325ae919c11bff
okekegodwin <okekegodwinobiora@gmail.com>
2024-03-20 06:05:51 +0100

    added new files and folders

---
+++ route/author.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const authorRouter = express.Router();
+
+authorRouter.get("/", (req, res) => {
+	res.json({ message: "Author / route" });
+});
+
+authorRouter.get("/:authorId", (req, res) => {
+	res.json({ name: "Harry potter", pages: 760 });
+});
+
+authorRouter.post("/", (req, res) => {
+	res.json({ message: "Post author route" });
+});
+
+authorRouter.put("/", (req, res) => {
+	res.json({ message: "Put author route" });
+});
+
+authorRouter.delete("/:authorId", (req, res) => {
+	res.json({ message: "Delete author route" });
+});
+
+module.exports = authorRouter;
